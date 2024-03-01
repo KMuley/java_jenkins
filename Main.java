8cbe16ce7a06efc3cec80acca313908078a96dd1
KMuley <kedar.muley0805@gmail.com>
2024-03-01 10:53:41 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main{
  public static void main(String[] args) {
      System.out.println("Java and Jenkins together");
      System.out.println("Is git repository working ??");
+     System.out.println("yes it seems to be working fine.");
      
  }
 }

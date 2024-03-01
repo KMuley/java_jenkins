63dd4f1aa99f6c010b10a1f7fabea129254c5c0d
KMuley <kedar.muley0805@gmail.com>
2024-03-01 14:11:22 +0530

    practice file changed

--- Practice.java
+++ Practice.java
@@ -1,5 +1,6 @@
 public class Practice {
     public static void main(String[] args) {
         System.out.println("This is a new Class");
+        System.out.println("Making changes in Practice demo file");
     }
 }
